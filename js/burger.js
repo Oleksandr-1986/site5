0e05f60b3300e7653adec5efeea0ce3c30ea81e9
Oleksandr-1986 <73231857+Oleksandr-1986@users.noreply.github.com>
2020-10-23 16:02:12 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function() {
+	$('.header__burger').click(function(event){
+		$('.header__burger,.header__top_nav').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
